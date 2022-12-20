3cf106c13d6682fe044ccd12d1ccfb1004693d95
Alina M <alinaamoskalenko@gmail.com>
2022-12-20 14:03:21 -0800

    ReactDOM updated to createRoot

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 function App() {
-  return <h1>Hello from the app component</h1>;
+  const title = 'Blog Post';
+  const body = 'This is my blog post!';
+  const comments = [
+    { id: 1, text: 'Comment one' },
+    { id: 2, text: 'Comment two' },
+    { id: 3, text: 'Comment three' },
+  ];
+
+  return (
+    <div className='container'>
+      <h1>{title}</h1>
+      <p>{body}</p>
+
+      <div className='comments'>
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment, idx) => (
+            <li key={idx}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
