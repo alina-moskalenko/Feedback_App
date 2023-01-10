0a9e7eb253793bbaecd5acba197a3bfc7dc7935b
Alina M <alinaamoskalenko@gmail.com>
2023-01-09 22:17:13 -0800

    css and first components added;

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
+import Header from './components/Header';
+
 function App() {
-  const title = 'Blog Post';
-  const body = 'This is my blog post!';
-  const comments = [
-    { id: 1, text: 'Comment one' },
-    { id: 2, text: 'Comment two' },
-    { id: 3, text: 'Comment three' },
-  ];
-
   return (
-    <div className='container'>
-      <h1>{title}</h1>
-      <p>{body}</p>
-
-      <div className='comments'>
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-          {comments.map((comment, idx) => (
-            <li key={idx}>{comment.text}</li>
-          ))}
-        </ul>
+    <>
+      <Header />
+      <div className='container'>
+        <h1>My App</h1>
       </div>
-    </div>
+    </>
   );
 }
 
