e7b50c87b5ce01b2320b644449084c5bece88a2d
Alina M <alinaamoskalenko@gmail.com>
2023-01-16 23:54:49 -0800

    feedback ids updated to strings (error fix)

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,16 +1,16 @@
 const FeedbackData = [
   {
-    id: 1,
+    id: '1',
     rating: 10,
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
   },
   {
-    id: 2,
+    id: '2',
     rating: 6,
     text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
   {
-    id: 3,
+    id: '3',
     rating: 5,
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tempus quam pellentesque nec nam aliquam sem.',
   },
